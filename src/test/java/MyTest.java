77a40b52c0f6c59575464d7c0e4882af53973e4a
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-18 20:13:08 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testBasic() {
+        assertEquals(9, Kata.squareSum(new int[]{1, 2, 2}));
+        assertEquals(5, Kata.squareSum(new int[]{1, 2}));
+        assertEquals(50, Kata.squareSum(new int[]{5, -3, 4}));
+    }
 
+    private static class Kata {
+        public static int squareSum(int[] n) {
+            int sum = 0;
+            for (int i = 0; i < n.length; i++) {
+                sum += n[i] * n[i];
+            }
+            return sum;
+        }
     }
 }
+
